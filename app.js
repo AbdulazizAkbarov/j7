fdb62aeb94c2624ac1e71faa0c8936963c70f61a
Abdulaziz <abdulazizakbarov065@gmail.com>
2024-10-23 15:15:46 +0500

    Slaom

---
+++ app.js
@@ -0,0 +1,107 @@
+// let arr =[1, 2, 3, 4, 5 ];
+// function sum(arr){
+//     let sum =0;
+//     for(let i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+// console.log((sum(arr)));
+
+
+
+// let arr =[1, 2, 3, 4, 5];
+// function aylana(arr){
+// let aylana =0;
+// }
+
+
+// 1 -misol
+// function num() {
+//     return Math.random() * (20 - 10 + 1) + 10;
+// }
+// alert(num());
+
+
+
+// 2-misol
+
+// let a = prompt("tug'ilgan sanangizni va kuni oyi soatini Kirgizing");
+// if (a) {
+//     let b = new Date(a);
+//     let c = new Date();
+
+//     let d = c - b;
+
+//     let seconds = Math.floor(d / 1000);
+//     let minutes = Math.floor(seconds / 60);
+//     let hours = Math.floor(minutes / 60);
+//     let days = Math.floor(d / (1000 * 60 * 60 * 24));
+
+//     let years = Math.floor(days / 365.25); 
+//     days %= 365.25; 
+
+//     let months = Math.floor(days / 30); 
+//     days %= 30;
+
+//     alert(`Siz ${years} yil, ${months} oy, ${days} kun, ${hours} soat, ${minutes} minut, ${seconds} sekund yashadingiz`);
+// } else {
+//     alert("Sana Yoq");
+// }
+
+//2-Tugashi//
+
+
+// 3-misol
+// let x =prompt("x-Sonini kirgizing")
+// let y =prompt("Y-Sonini kirgizing")
+//  function son(x, y){
+//     return Math.floor(Math.random() * (y - x +1))+x            
+//  }
+
+// let nam =son(x, y);
+// alert(`${x} ${y} Random Son => ${nam}`)
+
+//3-Tugashi///
+
+// 4-misol
+
+// let son =prompt("Ihtiyoriy Son Kirgizing");
+// function nam (son){
+//     return son.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+
+// } 
+// let a = nam(son);
+// alert(a);
+
+//4-Tugashi//
+
+
+// 5-misol
+
+// let soz =prompt ("istalgan so'zni kiritin")
+// function  soz(soz){
+//     let words = soz.split(' ');
+//     let count = 0;
+//     words.forEach (word => {
+//         if (word.charAt(0).toUpperCase() === word.charAt(0)){
+//             count++
+//         }
+//     });
+//     return count;
+// }
+// let a =soz(soz);
+// alert("Katta Hariflar soni " + a);
+
+
+
+
+
+
+  
+
+
+
+
+
+
